e77c67a45c67070076e71bf39fbe280c069787ca
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-08-31 18:54:27 +0300

    upload webview2

---
+++ WebviewDesktop/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace WebviewDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitBrowser();
+        }
+        private async Task initizated()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+        public async void InitBrowser()
+        {
+            await initizated();
+            webView21.CoreWebView2.Navigate("https://karadenizholding.com/");
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
